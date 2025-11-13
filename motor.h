5c38030a8d0c6dbe8bcab4a6de72b53b3a256d82
Dheeraj raju <dheshal07@gmail.com>
2025-11-13 17:57:08 +0530

    Refactor motor and light control functions
    Refactored motor and light control functions for clarity.

--- motor.h
+++ motor.h
@@ -1,15 +1,30 @@
-#ifndef __MOTOR_H__
-#define __MOTOR_H__
 #include <LPC21xx.h>
 
-#define MOTOR_PIN  (1<<7)
-#define LIGHT_PIN  (1<<8)
 
-static inline void motor_init(void) 
-{ IODIR0 |= MOTOR_PIN | LIGHT_PIN; }
-static inline void motor_on(void)  { IO0SET = MOTOR_PIN; }
-static inline void motor_off(void) { IO0CLR = MOTOR_PIN; }
-static inline void light_on(void)  { IO0SET = LIGHT_PIN; }
-static inline void light_off(void) { IO0CLR = LIGHT_PIN; }
+// P0.7 = MOTOR
+// P0.8 = LIGHT
 
+void control_init(void)
+{
+    IODIR0 |= (1<<7) | (1<<8);   // both outputs
+}
 
+void motor_on(void)
+{
+    IO0SET = (1<<7);
+}
+
+void motor_off(void)
+{
+    IO0CLR = (1<<7);
+}
+
+void light_on(void)
+{
+    IO0SET = (1<<8);
+}
+
+void light_off(void)
+{
+    IO0CLR = (1<<8);
+}

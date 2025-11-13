e27d7beaba0c5e75165c61556447724c65cad62e
Dheeraj raju <dheshal07@gmail.com>
2025-11-13 17:54:34 +0530

    Refactor ultrasonic sensor functions and cleanup

--- node1/ultrasonic.h
+++ node1/ultrasonic.h
@@ -1,44 +1,28 @@
-#ifndef __ULTRASONIC_H__
-#define __ULTRASONIC_H__
 #include <LPC21xx.h>
 #include "delay.h"
 
-#define TRIG_PIN (1<<10) // P0.10
-#define ECHO_PIN (1<<11) // P0.11
-
-static inline void ultrasonic_init(void)
+unsigned int get_distance(void)
 {
-    IODIR0 |= TRIG_PIN;   // trig out
-    IODIR0 &= ~ECHO_PIN;  // echo in
-}
+    unsigned int t;
 
-// Uses Timer0 microsecond tick from delay.h
-static inline unsigned int ultrasonic_distance_cm(void)
-{
-    unsigned int start, stop;
-    unsigned int timeout = 300000; // large timeout
-    // trigger
-    IO0CLR = TRIG_PIN;
+    IO0CLR = (1<<10);
     delay_us(2);
-    IO0SET = TRIG_PIN;
+    IO0SET = (1<<10);
     delay_us(10);
-    IO0CLR = TRIG_PIN;
+    IO0CLR = (1<<10);
 
-    // wait echo high
-    timeout = 300000;
-    while(!(IO0PIN & ECHO_PIN) && --timeout) { }
-    if(!timeout) return 0xFFFF;
+    t = 300000;
+    while(!(IO0PIN & (1<<11)))
+        if(--t == 0) return 999;
 
-    // measure width (use timer T0TC in us)
-    T0TCR = 0x02; // reset
-    T0TCR = 0x01; // start
-    timeout = 300000;
-    while((IO0PIN & ECHO_PIN) && --timeout) { }
-    stop = T0TC;
-    T0TCR = 0x00; // stop
+    T0TCR = 2;
+    T0TCR = 1;
 
-    // distance in cm: pulse_us/58
-    return (stop/58);
+    t = 300000;
+    while(IO0PIN & (1<<11))
+        if(--t == 0) break;
+
+    T0TCR = 0;
+
+    return T0TC/58;
 }
-
-#endif

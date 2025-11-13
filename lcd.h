8ead50d5a32599afcb9df6688f5617ffd3577136
Dheeraj raju <dheshal07@gmail.com>
2025-11-13 17:56:36 +0530

    Update lcd.h

--- lcd.h
+++ lcd.h
@@ -1,46 +1,31 @@
-#ifndef __LCD_H__
-#define __LCD_H__
 #include <LPC21xx.h>
 #include "delay.h"
 
-#define LCD_DATA_MASK  (0x00FF0000) // P1.16..P1.23
-#define LCD_RS         (1<<16)
-#define LCD_EN         (1<<17)
-
-static inline void lcd_cmd(unsigned char cmd)
+void lcd_cmd(unsigned char c)
 {
-    IO1CLR = LCD_DATA_MASK;
-    IO1SET = ((unsigned int)cmd << 16) & LCD_DATA_MASK;
-    IO1CLR = LCD_RS;
-    IO1SET = LCD_EN;
+    IO1CLR = 0x00FF0000;
+    IO1SET = (c << 16);
+    IO1CLR = (1<<16);  // RS=0
+    IO1SET = (1<<17);  // EN=1
     delay_ms(2);
-    IO1CLR = LCD_EN;
+    IO1CLR = (1<<17);
 }
 
-static inline void lcd_data(unsigned char data)
+void lcd_data(unsigned char d)
 {
-    IO1CLR = LCD_DATA_MASK;
-    IO1SET = ((unsigned int)data << 16) & LCD_DATA_MASK;
-    IO1SET = LCD_RS;
-    IO1SET = LCD_EN;
+    IO1CLR = 0x00FF0000;
+    IO1SET = (d << 16);
+    IO1SET = (1<<16);  // RS=1
+    IO1SET = (1<<17);
     delay_ms(2);
-    IO1CLR = LCD_EN;
+    IO1CLR = (1<<17);
 }
 
-static inline void lcd_init(void)
+void lcd_init(void)
 {
-    delay_ms(50);
+    delay_ms(20);
     lcd_cmd(0x38);
     lcd_cmd(0x0C);
     lcd_cmd(0x06);
     lcd_cmd(0x01);
-    delay_ms(5);
 }
-
-static inline void lcd_string(const char *s)
-{
-    while(*s) lcd_data(*s++);
-}
-
-
-

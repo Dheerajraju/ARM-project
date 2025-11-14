7d3df1c72c4970fff125a6f23806a24c82972ec4
Dheeraj raju <dheshal07@gmail.com>
2025-11-14 11:19:50 +0530

    Update delay.h

--- delay.h
+++ delay.h
@@ -4,21 +4,8 @@
 void delay_ms(unsigned int ms);
 
 #endif
-/*
-#include<LPC21XX.H>
-#define DELAY_H
 
-void delay_ms(unsigned int);
 
-void delay_ms(unsigned int ms)
-{
-    T0PR  = 15000-1;
-    T0TCR = 0x01;
-    while(T0TC < ms);
-    T0TCR = 0x03;
-    T0TCR = 0x00;
-}
-*/
 
 
 

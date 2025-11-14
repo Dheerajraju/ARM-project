b1078f230666563dc1d7d085e2f338aca459fdc0
Dheeraj raju <dheshal07@gmail.com>
2025-11-14 11:18:06 +0530

    Update main.c

--- node1/main.c
+++ node1/main.c
@@ -3,31 +3,39 @@
 #include "lcd.h"
 #include "can.h"
 #include "ultrasonic.h"
-
+#include "delay.c"
 int main()
 {
-    unsigned int d;
+    unsigned int dist;
 
-    IODIR1 |= 0x00FF0000 | (1<<16) | (1<<17);  
-    IODIR0 |= (1<<10);       
-    IODIR0 &= ~(1<<11);      
+    // LCD pins
+    IODIR1 |= 0x00FF0000 | (1<<16) | (1<<17);
 
-    delay_init();
-    lcd_init();
+    // Ultrasonic pins
+    IODIR0 |= (1<<10);       // TRIG
+    IODIR0 &= ~(1<<11);      // ECHO
+
     can_init();
+    lcd_init();
 
     while(1)
     {
-        d = get_distance();
+        dist = get_distance();
 
         lcd_cmd(0x80);
-        lcd_data((d/100)+'0');
-        lcd_data(((d/10)%10)+'0');
-        lcd_data((d%10)+'0');
+        lcd_data((dist/100) + '0');
+        lcd_data(((dist/10)%10) + '0');
+        lcd_data((dist%10) + '0');
 
-        if(d < 20)
-            can_send(1,1);      // ID=1, data=1
+        if(dist < 20)
+        {
+            can_send(1, 1);  // ID=1, obstacle found
+            lcd_cmd(0xC0);
+            lcd_data('O');
+            lcd_data('B');
+            lcd_data('S');
+        }
 
-        delay_ms(500);
+        delay_ms(300);
     }
 }

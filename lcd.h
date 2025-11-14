166171e20ccb2fa74e35c3289945e8e3ba2117e1
Dheeraj raju <dheshal07@gmail.com>
2025-11-14 11:20:49 +0530

    Update lcd.h

--- lcd.h
+++ lcd.h
@@ -1,21 +1,24 @@
 #include <LPC21xx.h>
 #include "delay.h"
+#define LCD_H
 
 void lcd_cmd(unsigned char c)
 {
     IO1CLR = 0x00FF0000;
     IO1SET = (c << 16);
-    IO1CLR = (1<<16);  // RS=0
-    IO1SET = (1<<17);  // EN=1
+
+    IO1CLR = (1<<16);   // RS = 0
+    IO1SET = (1<<17);   // EN = 1
     delay_ms(2);
-    IO1CLR = (1<<17);
+    IO1CLR = (1<<17);   // EN = 0
 }
 
 void lcd_data(unsigned char d)
 {
     IO1CLR = 0x00FF0000;
     IO1SET = (d << 16);
-    IO1SET = (1<<16);  // RS=1
+
+    IO1SET = (1<<16);   // RS = 1
     IO1SET = (1<<17);
     delay_ms(2);
     IO1CLR = (1<<17);

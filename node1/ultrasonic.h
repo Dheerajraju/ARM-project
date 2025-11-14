b17a757b003f53956631537b3e8b296315c60e5c
Dheeraj raju <dheshal07@gmail.com>
2025-11-14 11:18:26 +0530

    Update ultrasonic.h

--- node1/ultrasonic.h
+++ node1/ultrasonic.h
@@ -1,28 +1,31 @@
 #include <LPC21xx.h>
 #include "delay.h"
-
 unsigned int get_distance(void)
 {
     unsigned int t;
 
+    // Trigger pulse
     IO0CLR = (1<<10);
-    delay_us(2);
+    delay_ms(1);
     IO0SET = (1<<10);
-    delay_us(10);
+    delay_ms(1);
     IO0CLR = (1<<10);
 
-    t = 300000;
+    // Wait for echo HIGH
+    t = 50000;
     while(!(IO0PIN & (1<<11)))
         if(--t == 0) return 999;
 
-    T0TCR = 2;
-    T0TCR = 1;
+    // Measure pulse width using Timer1
+    T1PR = 14;     // approx 1us
+    T1TCR = 2;     // reset
+    T1TCR = 1;     // start
 
-    t = 300000;
+    t = 50000;
     while(IO0PIN & (1<<11))
         if(--t == 0) break;
 
-    T0TCR = 0;
+    T1TCR = 0;     // stop
 
-    return T0TC/58;
+    return T1TC / 58;  // convert to cm
 }
